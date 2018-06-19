57aae0048e2e779f40cf94849f35ab78bc68472c
ivan <ivan@example.com>
2018-06-19 14:58:50 +0200

    fin clase 19062018

--- src/app/Services/star-wars.service.ts
+++ src/app/Services/star-wars.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Planet } from '../domain/planet';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarWarsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPlanets():Observable<Planet[]>{
+    const url = "https://swapi.co/api/planets/";
+    return this.http.get(url,{})
+    .pipe(map(r => this.toPlanetArray(r)));
+
+  }
+
+  toPlanetArray(r:any):Planet[]{
+    return <Planet[]> r.results;
+  }
+
+  //getPlanet (id:string):Observable<Planet>{}
+  //getPeople()
+  //getFilms()
 }
--- src/app/planet-list/planet-list.component.ts
+++ src/app/planet-list/planet-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Planet } from '../domain/planet';
+import { map } from 'rxjs/operators';
+import { StarWarsService } from '../Services/star-wars.service';
+
 
 @Component({
   selector: 'app-planet-list',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planet-list.component.css']
 })
 export class PlanetListComponent implements OnInit {
+planets:Planet[];
+error:string;
 
-  constructor() { }
+
+  constructor(private stService:StarWarsService) { }
 
   ngOnInit() {
+    this.stService.getPlanets().subscribe(
+      data=>this.onData(data), 
+      error=>this.onError(error)
+    );
   }
 
+  private onData(data: Planet[]):void{
+this.planets =data;
+this.error = null;
+  }
+  private onError(error:any):void{
+    if (error && error.message){
+      this.error = error.message;
+    } else{
+      this.error = error.toString();
+    }
+  }
 }
